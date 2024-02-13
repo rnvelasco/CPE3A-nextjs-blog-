d83693c6f62ac32635837091b490bf4cd4a86098
Robert Velasco <rnvelasco@yahoo.com>
2024-02-13 20:52:02 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,16 +13,16 @@ export default function Home() {
           10 Things That Require Zero Talent
         </p>
         <ul>
-          <li>1. Being On Time</li>
-          <li>2. Making An Effort</li>
-          <li>3. Being High Energy</li>
-          <li>4. Having A Positive Attitude</li>
-          <li>5. Being Passionate</li>
-          <li>6. Using Good Body Language</li>
-          <li>7. Being Coachable</li>
-          <li>8. Doing A Little Extra</li>
-          <li>9. Being Prepared</li>
-          <li>10. Having A Strong Work Ethic</li>
+          <li>Being On Time</li>
+          <li>Making An Effort</li>
+          <li>Being High Energy</li>
+          <li>Having A Positive Attitude</li>
+          <li>Being Passionate</li>
+          <li>Using Good Body Language</li>
+          <li>Being Coachable</li>
+          <li>Doing A Little Extra</li>
+          <li>Being Prepared</li>
+          <li>Having A Strong Work Ethic</li>
         </ul>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
